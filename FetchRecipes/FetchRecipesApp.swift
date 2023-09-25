3880e997135f648df7f489ef8c4e6aa7ed1d7b4d
Jillian Scott <jilsco9@gmail.com>
2023-09-24 21:23:12 -0400

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Jillian Scott on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
